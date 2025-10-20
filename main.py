30675fa3ff0481ed8a7f0ba9aea77f037c20030f
bonemar8 <bonemar8@gmail.com>
2025-10-21 02:23:23 +0300

    total word count function created

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+def main():
+  path_for_frankenstein = "books/frankenstein.txt"
+
+  text = get_book_text(path_for_frankenstein)
+  word_count = get_total_word_count(text)
+  print(f"Found {word_count} total words")
+
 
 def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
+  
 
-def main():
-  path_for_frankenstein = "books/frankenstein.txt"
+def get_total_word_count(text):
+  return len(text.split())
 
-  content = get_book_text(path_for_frankenstein)
-  print(content)
 
 main()
